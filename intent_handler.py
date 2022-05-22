e184198d68312fd591fc1ad2fede695bd1a98a8d
javaojoe <jdavied883@gmail.com>
2022-05-22 22:31:09 +0100

    intent classifier

--- intent_handler.py
+++ intent_handler.py
@@ -1,14 +1,96 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import random as rand
-intents = ["greeting","goodbye","view_entry","add_entry"]
+import numpy as np
+import string
+import keras
+import pandas as pd
+import json
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+import pickle
+
+
+
+
+
+
+
+intents = ["greeting","bye","view_entry","add_entry", "yes", "no"]
 responses = {i:[] for i in intents}
 
 responses["greeting"].append("Hi {} how are you?")
 responses["greeting"].append("Hola {}")
 
-responses["goodbye"].append("Goodbye {}")
-responses["goodbye"].append("See ya later {}")
+responses["bye"].append("Goodbye {}")
+responses["bye"].append("See ya later {}")
+
+model = keras.models.load_model('intent_classifier.h5')
+
+def load_data():
+
+    data1 = json.loads(open('intents.json').read())
+    tags = []
+    inputs_list = []
+    responses = {}
+    ignore_letters = ['!', '?', '.', ',']
+
+    for inputs in data1:
+        inputs_list.append(inputs["Inputs"])
+        tags.append(inputs["Intents"])
+
+    data = pd.DataFrame({'inputs': inputs_list, 'tags': tags})
+
+    data['inputs'] = data['inputs'].apply(lambda w: [l.lower() for l in w if l not in string.punctuation])
+    data['inputs'] = data['inputs'].apply(lambda w: ''.join(w))
+
+    tokenwords = Tokenizer(num_words=2000)
+
+    tokenwords.fit_on_texts(data['inputs'])
+
+    labelencoding = LabelEncoder()
+    y = labelencoding.fit_transform(data['tags'])
+
+    tokenwords.word_index
+    labelencoding.classes_
+
+
+    outfile = open("tokenwords", 'wb')
+    pickle.dump(tokenwords, outfile)
+    outfile.close()
+
+
+    outfile = open("labelencoding", 'wb')
+    pickle.dump(tokenwords, outfile)
+    outfile.close()
+
+    return tokenwords, labelencoding
+
 def get_intent(user_input):
-    return intents[3]
+    list_text = []
+
+    tokenwords , labelencoding= load_data()
+
+    make_prediction = [letters.lower() for letters in user_input if letters not in string.punctuation]
+    make_prediction = ''.join(make_prediction)
+    list_text.append(make_prediction)
+
+    make_prediction = tokenwords.texts_to_sequences(list_text)
+    make_prediction = np.array(make_prediction).reshape(-1)
+    make_prediction = pad_sequences([make_prediction], 16)
+
+    lstm_p = model.predict(make_prediction)
+
+    lstm_p = lstm_p.argmax()
+
+    preidicted_response_tag = labelencoding.inverse_transform([lstm_p])[0]
+
+    return preidicted_response_tag
 
 def get_response_by_intent(intent):
+
+
     return rand.choice(responses[intent])
+
+
--- ner_handler.py
+++ ner_handler.py
@@ -1,10 +1,9 @@
-#Try grouping potentially split up entities
+# Try grouping potentially split up entities
 import logging
 import joblib
 import calendar
 
 logging.basicConfig(filename='ner.log', encoding='utf-8', level=logging.INFO)
-
 
 import torch
 import re
@@ -13,37 +12,40 @@ from simpletransformers.ner import NERModel
 cuda_available = torch.cuda.is_available()
 
 model = joblib.load("ner_classifier_nmp.joblib")
-      
-new_labels_enum = {"PER":"PERSON","LOC":"GPE","ORG":"ORGANIZATION","MISC":"MISCELLANEOUS"}
+
+new_labels_enum = {"PER": "PERSON", "LOC": "GPE", "ORG": "ORGANIZATION", "MISC": "MISCELLANEOUS"}
 begin_ent = re.compile("B-[A-Za-z]+")
+
+
 def group_split_entities(predictions):
-  entities = {}
-  list_prediction = list(predictions[0])
-  for i in range(0,len(list_prediction)):
-    end_entity = ""
-    list_ent = list_prediction[i]
-    entity = list(list_ent.keys())[0]
-    if list_ent[entity] == "O" or re.match("I-[A-Za-z]+",list_ent[entity]):
-      continue
-    elif begin_ent.match(list_ent[entity]):
-        end_entity = list_ent[entity][2:]
-        valid_ent=True
-        new_entity = entity
-        i=i+1
-        while i< len(list_prediction) and valid_ent:
-          list_ent=list_prediction[i]
-          entity = list(list_ent.keys())[0]
-          if re.match("I-"+end_entity,list_ent[entity]):
-            new_entity = new_entity+" "+entity
-            i+=1
-          else:
-            valid_ent = False        
-        entities = {**entities, new_entity:new_labels_enum[end_entity]}
-        
-    else:
-      entities = {**entities,entity:list_ent[entity]}
-    
-  return entities
+    entities = {}
+    list_prediction = list(predictions[0])
+    for i in range(0, len(list_prediction)):
+        end_entity = ""
+        list_ent = list_prediction[i]
+        entity = list(list_ent.keys())[0]
+        if list_ent[entity] == "O" or re.match("I-[A-Za-z]+", list_ent[entity]):
+            continue
+        elif begin_ent.match(list_ent[entity]):
+            end_entity = list_ent[entity][2:]
+            valid_ent = True
+            new_entity = entity
+            i = i + 1
+            while i < len(list_prediction) and valid_ent:
+                list_ent = list_prediction[i]
+                entity = list(list_ent.keys())[0]
+                if re.match("I-" + end_entity, list_ent[entity]):
+                    new_entity = new_entity + " " + entity
+                    i += 1
+                else:
+                    valid_ent = False
+            entities = {**entities, new_entity: new_labels_enum[end_entity]}
+
+        else:
+            entities = {**entities, entity: list_ent[entity]}
+
+    return entities
+
 
 def predict_ner(user_input):
     logging.info("Making Prediction")
@@ -52,64 +54,68 @@ def predict_ner(user_input):
     logging.info('\nNER PREDICTION \nUser input: %s\n Predicted Entities: %s', user_input, str(grouped_entities))
     return grouped_entities
 
-def get_entity(user_input,entity_type):
-  predictions = predict_ner(user_input)
-  people = [k for k in predictions.keys() if predictions[k]==entity_type]
-  return people
+
+def get_entity(user_input, entity_type):
+    predictions = predict_ner(user_input)
+    people = [k for k in predictions.keys() if predictions[k] == entity_type]
+    return people
 
 
 from datetime import date, timedelta
 import difflib
 
-def search_by_weekday(predictions,input_split):
-  last_week = False
-  error = False
-  weekday = -1
-  target_date = ""
-  if 'week' in predictions.keys():
-      pre_week_pos = input_split.index('week')-1
-      if pre_week_pos>-1 and input_split[pre_week_pos].lower() == "last":
-        last_week = True
 
-  if last_week:
-    del predictions['week']
-    
-  for key in predictions.keys():
-    predictions[key]
-    if predictions[key] =='DATE':
-      days_of_week =[day.upper() for day in calendar.day_name]
-      day = difflib.get_close_matches(key.upper(), days_of_week)[0]
-      weekday = days_of_week.index(day)
-      break
+def search_by_weekday(predictions, input_split):
+    last_week = False
+    error = False
+    weekday = -1
+    target_date = ""
+    if 'week' in predictions.keys():
+        pre_week_pos = input_split.index('week') - 1
+        if pre_week_pos > -1 and input_split[pre_week_pos].lower() == "last":
+            last_week = True
 
-  today = date.today()
-  time_delta=timedelta(days=-today.weekday()+weekday)
-  start = today + time_delta
-  if not last_week:
-    potential_days = list(filter(lambda v: re.match('([0-9])', v), input_split))
-    if len(potential_days)>0:
-      target = today - timedelta(days=int(potential_days[0]))
+    if last_week:
+        del predictions['week']
+
+    for key in predictions.keys():
+        predictions[key]
+        if predictions[key] == 'DATE':
+            days_of_week = [day.upper() for day in calendar.day_name]
+            day = difflib.get_close_matches(key.upper(), days_of_week)[0]
+            weekday = days_of_week.index(day)
+            break
+
+    today = date.today()
+    time_delta = timedelta(days=-today.weekday() + weekday)
+    start = today + time_delta
+    if not last_week:
+        potential_days = list(filter(lambda v: re.match('([0-9])', v), input_split))
+        if len(potential_days) > 0:
+            target = today - timedelta(days=int(potential_days[0]))
+        else:
+            offset = (today.weekday() - weekday) % 7
+            target = today - timedelta(days=offset)
+
     else:
-      offset = (today.weekday() - weekday) % 7
-      target = today - timedelta(days=offset)
-    
-  else:
-    target = start - timedelta(days=7)
-  target_date = str(target)
+        target = start - timedelta(days=7)
+    target_date = str(target)
 
-#weekday==-1 and 
-  return target_date
+    # weekday==-1 and
+    return target_date
+
 
 def show_last_entry(user_input):
-  print("What entry do you want to see")
-  print(user_input)
-  predictions = predict_ner(user_input)
-  input_split = user_input.split(" ")
-  potential_dates = list(filter(lambda v: re.match('(^(0[1-9]|[12][0-9]|3[01])(-|\/)(0[1-9]|1[0-2])(-|\/)\d{4}$)', v), input_split))
-  target_date = ""
-  if len(potential_dates)>0:
-    target_date=potential_dates[0]
-  else:
-    target_date = search_by_weekday(predictions,input_split)
+    print("What entry do you want to see")
+    print(user_input)
+    predictions = predict_ner(user_input)
+    input_split = user_input.split(" ")
+    potential_dates = list(
+        filter(lambda v: re.match('(^(0[1-9]|[12][0-9]|3[01])(-|\/)(0[1-9]|1[0-2])(-|\/)\d{4}$)', v), input_split))
+    target_date = ""
+    if len(potential_dates) > 0:
+        target_date = potential_dates[0]
+    else:
+        target_date = search_by_weekday(predictions, input_split)
 
-  return target_date
+    return target_date
